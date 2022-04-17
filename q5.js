401380d2e72ce68d13507f8d8a47d4a6bbd302b3
Nitesh Kumar <82534120+Nitesh8877@users.noreply.github.com>
2022-04-18 01:36:29 +0530

    Update q5.js

--- q5.js
+++ q5.js
@@ -1,1 +1,17 @@
-var a='hello'
+//Convert miles Meter to Kilometers;
+
+function convertMilesToKM(arr){
+    return arr.map(mile);
+    function mile(a){
+        return a*1.609;
+    }
+    
+}
+function totalDistance(acc,cur){
+    return acc+cur;
+}
+const km=convertMilesToKM([1,2,3,4]);
+console.log(km);
+let totaldistanceKM=km.reduce(totalDistance);
+
+console.log(totaldistanceKM);

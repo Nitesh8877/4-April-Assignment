d90d8a76f802612867a4c33b308a2c4d369d8d0e
Nitesh Kumar <82534120+Nitesh8877@users.noreply.github.com>
2022-04-18 01:54:52 +0530

    Update q3.js

--- q3.js
+++ q3.js
@@ -1,1 +1,17 @@
-var a=56;
+let input=10101;
+
+const ConvertBinary=(binary)=>{
+
+    let i=0;
+    let sum=0;
+    let nums=binary;
+    while(nums){
+        sum+=Math.pow(2,i)*(nums%10);
+        i++;
+        nums=parseInt(nums/10);
+    }
+    return sum;
+}
+
+let output=ConvertBinary(input);
+console.log(output);
